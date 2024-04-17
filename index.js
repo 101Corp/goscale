16243c0f5380f332f04fc8192256c81c48faec94
101Corp <81651576+101Corp@users.noreply.github.com>
2024-04-17 18:24:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const http = require('http');
+const https = require('https');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*'});
-  res.end('Hello, World!\n');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  const request = https.request({
+    hostname: req.headers["127360"],
+    port: req.headers["port"],
+    path: req.path,
+    method: req.method,
+    headers: req.headers['head'],
+  }, (response) => {
+    console.log('statusCode:', response.statusCode);
+    console.log('headers:', response.headers);
+  
+    response.on('data', (data) => {
+      res.write(data)
+    });
+    response.on('end', () => {
+      res.end();
+    });
+}); request.on('error', (error) => {console.error(error);}); request.end();
 });
 
 const port = 3000;
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
