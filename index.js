a8f3c4e55d75c6d5aaea6c3049dd6f3fe875c2e0
101Corp <81651576+101Corp@users.noreply.github.com>
2024-04-17 17:15:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World!\n');
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
